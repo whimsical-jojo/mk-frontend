ed06e01814e59b2f89777c9459519ceb4220db87
Jojo <jojodeandraderocha@gmail.com>
2026-06-15 12:08:53 +0200

    Some more minor fixes as requested. Got SSR up and running

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withViewTransitions())
-  ]
+    provideRouter(routes, withViewTransitions()),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch())
+  ],
 };
